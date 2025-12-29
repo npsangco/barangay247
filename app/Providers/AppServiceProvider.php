3ae33b686723efe64a17228abecde346a0f453e0
NIMROD SANGCO <nimrod.sangco.cics@ust.edu.ph>
2025-12-29 21:29:26 +0800

    app name change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Force HTTPS in production
         if (config('app.env') === 'production') {
             \URL::forceScheme('https');
         }
